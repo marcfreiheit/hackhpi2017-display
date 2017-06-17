06a08f23d4ac8262513cb356823eb75971be6057
Marc André Freiheit <marcandre@freiheit.software>
2017-06-17 20:31:46 +0200

    text #2 - bug fixing

--- led.py
+++ led.py
@@ -1,7 +1,3 @@
-import re
-import time
-import argparse
-
 from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
@@ -10,14 +6,12 @@ from luma.core.legacy import text, show_message
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
 
 def displayInfo(text):
-    global str
-    
     # create matrix device
     serial = spi(port=0, device=0, gpio=noop())
     device = max7219(serial, width=32, height=32, block_orientation=-90)
 
     with canvas(device) as draw:
-        text(draw, (0, -1), str(text), fill="white", font=proportional(TINY_FONT))
+        text(draw, (0, -1), text, fill="white", font=proportional(TINY_FONT))
         #text(draw, (3, 8), "2017", fill="white", font=proportional(SINCLAIR_FONT))
         #text(draw, (2, 18), "1.92m", fill="white", font=proportional(SINCLAIR_FONT))
         # draw.line()
