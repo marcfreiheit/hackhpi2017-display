f0cb2e4e1f2bf1bcc513ab95cbc235b46c441c03
Marc André Freiheit <marcandre@freiheit.software>
2017-06-17 20:33:44 +0200

    test #2 - fuck my life

--- led.py
+++ led.py
@@ -5,13 +5,13 @@ from luma.core.virtual import viewport # allow scrolling support
 from luma.core.legacy import text, show_message
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
 
-def displayInfo(text):
+def displayInfo(param):
     # create matrix device
     serial = spi(port=0, device=0, gpio=noop())
     device = max7219(serial, width=32, height=32, block_orientation=-90)
 
     with canvas(device) as draw:
-        text(draw, (0, -1), "Hello", fill="white", font=proportional(TINY_FONT))
+        text(draw, (0, -1), param, fill="white", font=proportional(TINY_FONT))
         #text(draw, (3, 8), "2017", fill="white", font=proportional(SINCLAIR_FONT))
         #text(draw, (2, 18), "1.92m", fill="white", font=proportional(SINCLAIR_FONT))
         # draw.line()
