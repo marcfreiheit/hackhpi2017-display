8b9953838e737094f1bc5ae1bfdf779266df5b27
Marc André Freiheit <marcandre@freiheit.software>
2017-06-17 20:23:28 +0200

    test #2 - rename file to call from other file

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import bottle
 from bottle import route, run, request, template, static_file, post, get
 import os
 import json
-import child-growth-info
+import led
 
 base_path = os.path.abspath(os.path.dirname(__file__))
 view_path = os.path.join(base_path, 'views')
@@ -10,6 +10,6 @@ bottle.TEMPLATE_PATH.insert(0, view_path)
 
 @route('/display/<text>')
 def do_display(text):
-    child-growth-info.displayInfo(text)
+    led.displayInfo(text)
 
 run(host='localhost', port=8080, debug=True)
