6cc2c1f68d89463422fc6c72fb786bf4a4a2c2a0
Marc André Freiheit <marcandre@freiheit.software>
2017-06-17 20:44:34 +0200

    test #3 - fix bug

--- led.py
+++ led.py
@@ -8,6 +8,9 @@ from luma.core.legacy import text, show_message
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
 
 def displayInfo(height):
+    # get now
+    now = datetime.datetime.now()
+
     # create matrix device
     serial = spi(port=0, device=0, gpio=noop())
     device = max7219(serial, width=32, height=32, block_orientation=-90)
