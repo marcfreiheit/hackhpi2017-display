a88424d0b4cb1f375827290611b44e7d3ee722be
Marc André Freiheit <marcandre@freiheit.software>
2017-06-17 20:24:07 +0200

    test #2 - don't know

--- led.py
+++ led.py
@@ -9,7 +9,7 @@ from luma.core.virtual import viewport # allow scrolling support
 from luma.core.legacy import text, show_message
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
 
-def displayInfo(text):
+def displayInfo(self, text):
     # create matrix device
     serial = spi(port=0, device=0, gpio=noop())
     device = max7219(serial, width=32, height=32, block_orientation=-90)
