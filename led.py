d9630b9c66179a3340c7568954d24d34f5322f6a
Marc André Freiheit <marcandre@freiheit.software>
2017-06-17 20:29:49 +0200

    test #2 - stackoverflow

--- led.py
+++ led.py
@@ -10,6 +10,8 @@ from luma.core.legacy import text, show_message
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
 
 def displayInfo(text):
+    global str
+    
     # create matrix device
     serial = spi(port=0, device=0, gpio=noop())
     device = max7219(serial, width=32, height=32, block_orientation=-90)
