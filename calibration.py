d9b1ad3a4dbb56f57e2c740ef9bd9ddcdff54280
Marc André Freiheit <marcandre@freiheit.software>
2017-06-18 02:01:38 +0200

    top right

--- calibration.py
+++ calibration.py
@@ -20,5 +20,6 @@ def calibrate():
     LEDMatrix.gfx_set_px(8,0, GFX_ON)
     LEDMatrix.gfx_set_px(0,8, GFX_ON)
     LEDMatrix.gfx_set_px(0,24, GFX_ON)
+    LEDMatrix.gfx_set_px(32,8, GFX_ON)
     # LEDMatrix.gfx_sprite_array(matrix, 0, 0, GFX_ON)
     LEDMatrix.gfx_render()
